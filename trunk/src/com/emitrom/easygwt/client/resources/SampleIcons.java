75625fc6c7976d99d243531268e4e532c9e0a322
lawwton@gmail.com <lawwton@gmail.com@d92be766-8be0-fc86-d09b-d7d3c5131dc4>
2011-06-15 15:38:37 +0000

    - Added a few more icons to decorate the Sample App.

--- trunk/src/com/emitrom/easygwt/client/resources/SampleIcons.java
+++ trunk/src/com/emitrom/easygwt/client/resources/SampleIcons.java
@@ -7,5 +7,21 @@ public interface SampleIcons extends ClientBundle {
 
 	@Source("icons/chart_bar.png")
 	ImageResource chartBar();
+	
+	@Source("icons/coins.png")
+	ImageResource coins();
+	
+	@Source("icons/group.png")
+	ImageResource group();
+	
+	@Source("icons/house.png")
+	ImageResource house();
+
+	@Source("icons/user_comment.png")
+	ImageResource userComment();
+
+	@Source("icons/user.png")
+	ImageResource user();
+
 
 }
