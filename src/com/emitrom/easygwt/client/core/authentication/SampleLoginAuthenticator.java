f4c48b2017aa4378fb55c18c58bf1a79ce61647b
lawwton <lawwton@gmail.com>
2011-06-26 13:19:42 +0000

    - Adding remember me functionality to the sample app.

--- src/com/emitrom/easygwt/client/core/authentication/SampleLoginAuthenticator.java
+++ src/com/emitrom/easygwt/client/core/authentication/SampleLoginAuthenticator.java
@@ -2,6 +2,7 @@ package com.emitrom.easygwt.client.core.authentication;
 
 import com.emitrom.easygwt.wf.client.authentication.Authenticator;
 import com.emitrom.easygwt.wf.client.widgets.dialogs.LoginDialog;
+import com.extjs.gxt.ui.client.state.StateManager;
 
 public abstract class SampleLoginAuthenticator implements Authenticator {
 
@@ -18,6 +19,7 @@ public abstract class SampleLoginAuthenticator implements Authenticator {
 		String passWord = loginDialog.getPasswordTextField().getValue();
 		
 		if (userName.equals("demo@easy-gwt.com") && passWord.equals("demo")) {
+			StateManager.get().set("remember_me", userName);
 			onSuccess();
 		} else {
 			loginDialog.getLoginFailureMessageLabelField().setVisible(true);
