b7123b55b1d82349b6365eb6afff25502bc38b25
lawwton@gmail.com <lawwton@gmail.com@d92be766-8be0-fc86-d09b-d7d3c5131dc4>
2011-06-29 23:15:45 +0000

    - Getting the Sample App ready for prime time. - A few things still to come for the Wizard.

--- trunk/src/com/emitrom/easygwt/client/ApplicationController.java
+++ trunk/src/com/emitrom/easygwt/client/ApplicationController.java
@@ -71,40 +71,15 @@ public class ApplicationController implements EntryPoint {
 				List<ColumnNavigationParent> navigationParentsList = new ArrayList<ColumnNavigationParent>();
 				
 				ColumnNavigationParent parent1 = new ColumnNavigationParent();
-				parent1.setHeading(constants.parentOne());
+				parent1.setHeading(constants.usersViewHeading());
 				parent1.setIcon(AbstractImagePrototype.create(icons.chartBar()));
 				
-				ColumnNavigationChild parent1Child1 = new ColumnNavigationChild(injector.getMyView(), 
-						constants.parentOneChildOne());
+				ColumnNavigationChild parent1Child1 = new ColumnNavigationChild(injector.getUsersView(), 
+						constants.usersViewHeading());
 				parent1Child1.setIcon(icons.house());
 				parent1.addNavigationChild(parent1Child1);
 				
-				ColumnNavigationChild parent1Child2 = new ColumnNavigationChild(injector.getMyView(), 
-						constants.parentOneChildTwo());
-				parent1Child2.setIcon(icons.coins());
-				parent1.addNavigationChild(parent1Child2);
-
-				ColumnNavigationParent parent2 = new ColumnNavigationParent();
-				parent2.setHeading(constants.parentTwo());
-				parent2.setIcon(AbstractImagePrototype.create(icons.group()));
-				
-				ColumnNavigationChild parent2Child1 = new ColumnNavigationChild(injector.getMySecondView(), 
-						constants.parentTwoChildOne());
-				parent2Child1.setIcon(icons.user());
-				parent2.addNavigationChild(parent2Child1);
-				
-				ColumnNavigationChild parent2Child2 = new ColumnNavigationChild(injector.getMyView(), 
-						constants.parentTwoChildTwo());
-				parent2Child2.setIcon(icons.userComment());
-				parent2.addNavigationChild(parent2Child2);
-				
-				ColumnNavigationChild parent2Child3 = new ColumnNavigationChild(injector.getMyErrorDialogView(),
-				        constants.parentTwoChildThree());
-				parent2Child3.setIcon(icons.coins());
-				parent2.addNavigationChild(parent2Child3);
-
 				navigationParentsList.add(parent1);
-				navigationParentsList.add(parent2);
 
 				columnView.addNavigationItems(navigationParentsList);
 
--- trunk/src/com/emitrom/easygwt/client/core/injection/SampleInjector.java
+++ trunk/src/com/emitrom/easygwt/client/core/injection/SampleInjector.java
@@ -1,16 +1,12 @@
 package com.emitrom.easygwt.client.core.injection;
 
-import com.emitrom.easygwt.client.views.MyErrorDialogView;
-import com.emitrom.easygwt.client.views.MySecondView;
-import com.emitrom.easygwt.client.views.MyView;
+import com.emitrom.easygwt.client.views.UsersView;
 import com.google.gwt.inject.client.GinModules;
 import com.google.gwt.inject.client.Ginjector;
 
 @GinModules({SampleBinder.class})
 public interface SampleInjector extends Ginjector {
 
-	MyView getMyView();
-	MySecondView getMySecondView();
-	MyErrorDialogView getMyErrorDialogView();
+	UsersView getUsersView();
 	
 }
