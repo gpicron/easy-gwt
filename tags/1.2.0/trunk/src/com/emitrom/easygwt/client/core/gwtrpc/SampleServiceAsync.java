a2931204b138338afb764b2562c35ec80aa3accf
lawwton@gmail.com <lawwton@gmail.com@d92be766-8be0-fc86-d09b-d7d3c5131dc4>
2012-05-10 20:18:41 +0000

    - Wrong copy

--- tags/1.2.0/trunk/src/com/emitrom/easygwt/client/core/gwtrpc/SampleServiceAsync.java
+++
@@ -1,9 +0,0 @@
-package com.emitrom.easygwt.client.core.gwtrpc;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface SampleServiceAsync {
-
-	public void sampleRPC(String string, AsyncCallback<String> asyncCallback);
-	
-}
